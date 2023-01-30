c56d0fcd92ebc42b0943ded74016b11b204895ea
박규태 <79373803+KyuTae98@users.noreply.github.com>
2023-01-30 18:50:26 +0900

    [level 2] Title: 구명보트, Time: 32.99 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,14 +1,31 @@
 function solution(people, limit) {
-    let start = 0;
-    let end = people.length-1;
+    var answer = 0;
+    let load = 0;
     let count = 0;
     people.sort((a,b)=>a-b);
-    while(end>=start){
-        if(people[end]+people[start]<=limit)
-            start++;
-        end--;
-        count++;
+    for(let i=0;i<people.length;i++){
+        let current = people[i];
+        if(current>Math.floor(limit/2)){//반 초과 무게 있을때
+            answer+=people.length-i;
+            break;
+        }
+        else if(people.slice(i+1,people.length).includes(limit-current)){//limit에 딱 맞는 수가 있을경우
+            answer++;
+            people.splice(i,1);
+            people.splice(people.indexOf(limit-current),1);
+            i--;
+        }
+        else if(load+current<=limit){
+            load+=current;
+            count++;
+        }
+        if(count===2){
+            load=0;
+            count=0;
+            answer++;
+        }
+        
     }
-    return count;
+    return answer+count;
 }
 //인원제한... 2명
