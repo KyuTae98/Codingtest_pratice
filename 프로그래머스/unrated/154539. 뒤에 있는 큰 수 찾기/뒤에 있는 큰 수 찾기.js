81cced530ed68f585395381b88abb3d92c45d7ea
박규태 <79373803+KyuTae98@users.noreply.github.com>
2023-02-15 14:04:07 +0900

    [unrated] Title: 뒤에 있는 큰 수 찾기, Time: 40.15 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+    let answer = new Array(numbers.length).fill(-1);
+    let stack = [];
+    for(let i=0;i<numbers.length;i++){
+        while(numbers[stack[stack.length-1]]<numbers[i])
+            answer[stack.pop()] = numbers[i];
+        stack.push(i);
+    }
+    return answer;
+}
