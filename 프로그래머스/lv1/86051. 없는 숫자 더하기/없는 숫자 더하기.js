03e9a4dca7ad8c53e5bfae3b862abedfbe28e2cb
박규태 <79373803+KyuTae98@users.noreply.github.com>
2023-08-26 22:34:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,8 @@
+function solution(numbers) {
+    var answer = 0;
+    let nums = [1,2,3,4,5,6,7,8,9]
+    for(let i=0;i<nums.length;i++)
+        if(!(numbers.includes(nums[i])))
+            answer+=nums[i]
+    return answer;
+}
