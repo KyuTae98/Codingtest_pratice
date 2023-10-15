966b8d9eac1340299e33aab02cfa9d096b17b5a5
박규태 <79373803+KyuTae98@users.noreply.github.com>
2023-10-15 16:21:28 +0900

    [level 2] Title: 요격 시스템, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -0,0 +1,27 @@
+function solution(targets) {
+    let answer = 0;
+    let r = [0,0];
+    targets.sort((a,b)=>{
+        if(a[0]===b[0])
+            return b[1]-a[1];
+        return a[0]-b[0];
+    });
+    for(let i=0;i<targets.length;i++){
+        let [r1, r2] = r;
+        let [cur_t1, cur_t2] = targets[i];
+        if(r1<=cur_t1&&r2>cur_t1){//target의 시작을 기준
+            if(cur_t2>r2)
+                r = [cur_t1,r2];
+            else
+                r = [cur_t1,cur_t2]
+        }
+        else{
+            r = targets[i]
+            answer++;
+        }
+    }
+    return answer;
+}
+/*
+정수가 아니어도 요격가능
+*/
